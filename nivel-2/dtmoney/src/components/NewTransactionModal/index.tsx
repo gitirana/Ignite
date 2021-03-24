72fe869c4f92761310544f763be57dd426171aec
gitirana <tlgc@poli.br>
2021-03-23 21:52:32 -0300

    feat: moving new transaction creation code

--- nivel-2/dtmoney/src/components/NewTransactionModal/index.tsx
+++ nivel-2/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,13 @@
+import { FormEvent, useContext, useState } from "react";
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { FormEvent, useState } from "react";
-import { api } from "../../services/api";
+
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+
+import { TransactionsContext } from "../../TransactionsContext";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -17,16 +19,21 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  const { createTransaction } = useContext(TransactionsContext);
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = { title, value, category, type };
-
-    api.post("/transactions", data);
+    createTransaction({
+      title,
+      amount,
+      category,
+      type,
+    });
   }
 
   return (
@@ -55,8 +62,8 @@ export function NewTransactionModal({
         <input
           placeholder="Valor"
           type="number"
-          value={value}
-          onChange={(event) => setValue(+event.target.value)}
+          value={amount}
+          onChange={(event) => setAmount(+event.target.value)}
         />
 
         <TransactionTypeContainer>
